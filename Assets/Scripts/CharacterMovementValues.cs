cfd2f99a436216743852017c8a79fe28087efe7c
Hugo Guerquin <hugo.guerquin@outlook.fr>
2022-02-02 14:37:21 +0100

    Copy paste frnm Unity + start self made one

--- Assets/Scripts/CharacterMovementValues.cs
+++ Assets/Scripts/CharacterMovementValues.cs
@@ -28,6 +28,7 @@ public class CharacterMovementValues : ScriptableObject
 	public float JumpHeight = 1.2f;
 	[Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
 	public float Gravity = -15.0f;
+	public float TerminalVelocity = 53.0f;
 
 	[Space(10)]
 	[Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
